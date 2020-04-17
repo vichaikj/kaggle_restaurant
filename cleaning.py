ca84ab1c80864f8467c09c953ff3601d10d40961
vichaikj <jean-philippe.vichaikit@edu.esiee.fr>
2020-04-17 19:00:50 +0200

    update cleaning

--- cleaning.py
+++ cleaning.py
@@ -4,21 +4,21 @@ import numpy as np
 import math
 from datetime import datetime
 from datetime import date
-from translate import Translator
+import category_encoders as ce
 
 
-def merge_dataframe():
+def merge_dataframe(file):
     """
-        Merge des dataframes de member.csv, restaurant.csv et Train.csv
+        Merge des dataframes de member.csv, restaurant.csv et Train.csv / Test.csv
     """
     df_member = pd.read_csv("member.csv", encoding="UTF-16", sep="\t")
     df_restaurant = pd.read_csv("restaurant.csv", encoding="UTF-16", sep="\t")
-    df_train = pd.read_csv("Train.csv", encoding="UTF-8", sep=",")
+    df_train = pd.read_csv(file, encoding="UTF-8", sep=",")
 
     # renommer certaines colonnes pour les left join
     df_member = df_member.rename(columns={"id":"member_id", "cdate":"cdate_member", "city":"city_member"})
     df_restaurant = df_restaurant.rename(columns={"id":"restaurant_id", "cdate":"cdate_restaurant", "city":"city_restaurant"})
-    df_train = df_train.rename(columns={"cdate":"cdate_train"})
+    df_train = df_train.rename(columns={"cdate":"cdate_" + file[:-4].lower()})
 
     # left join de Train à member
     result = pd.merge(df_train, df_member, on="member_id", how="left")
@@ -26,14 +26,13 @@ def merge_dataframe():
     # left join de result à restaurant
     df = pd.merge(result, df_restaurant, on="restaurant_id", how="left")
 
-    # on enlève les NaN, ...
-    # df = df.dropna()
 
     # drop la latitude et longitude car donnees redondantes
     # drop de name, abbr et tel car donnees anonymisees
     # drop de restaurant_id et member_it car donnees anonymisees
-    # drop de cityarea car trop de données manquantes
-    df = df.drop(["lat", "lng", "name", "abbr", "tel", "cityarea"], axis=1)
+    # drop de cityarea et city_member car trop de données manquantes
+    # drop de timezone et locale car données récurentes (on sait déjà que c'est à Taiwan...)
+    df = df.drop(["lat", "lng", "name", "abbr", "tel", "cityarea", "city_member", "opening_hours", "timezone", "locale"], axis=1)
 
     return df
 
@@ -64,8 +63,80 @@ def date_format(df):
     """
         Applique la fonction change_date_format() à cdate_train et datetime
     """
-    df["cdate_train"] = change_date_format(df["cdate_train"])
+    try:
+        df["cdate_train"] = change_date_format(df["cdate_train"])
+    except:
+        df["cdate_test"] = change_date_format(df["cdate_test"])
+
     df["datetime"] = change_date_format(df["datetime"])
+
+    return df
+
+
+def drop_hours(df):
+    """
+        Drop l'heure et les minutes des dates (cdate_train/test, datetime, cdate_member, cdate_restaurant) pour une question de simplicité
+    """
+
+    cdate_train = []
+    cdate_test = []
+    datetime = []
+    cdate_member = []
+    cdate_restaurant = []
+
+    try:
+        for elt in df["cdate_train"]:
+            cdate_train.append(elt.split(" ")[0])
+    except:
+        for elt in df["cdate_test"]:
+            cdate_test.append(elt.split(" ")[0])
+    for elt in df["datetime"]:
+        datetime.append(elt.split(" ")[0])
+    for elt in df["cdate_member"]:
+        cdate_member.append(elt.split(" ")[0])
+    for elt in df["cdate_restaurant"]:
+        cdate_restaurant.append(elt.split(" ")[0])
+
+    try:
+        df["cdate_train"] = cdate_train
+    except:
+        df["cdate_test"] = cdate_test
+    df["datetime"] = datetime
+    df["cdate_member"] = cdate_member
+    df["cdate_restaurant"] = cdate_restaurant
+
+    return df
+
+
+def split_date(df):
+    """
+        Sépare les jours, mois et années dans différentes colonnes pour chaque date
+    """
+
+    date_list = ['cdate_train', 'datetime', 'birthdate', 'cdate_member', 'cdate_restaurant']
+
+
+    for column in date_list:
+        month = []
+        day = []
+        year = []
+        try:
+            for elt in df[column]:
+                month.append(float(elt.split("/")[0]))
+                day.append(float(elt.split("/")[1]))
+                year.append(float(elt.split("/")[2]))
+        except:
+            for elt in df["cdate_test"]:
+                month.append(float(elt.split("/")[0]))
+                day.append(float(elt.split("/")[1]))
+                year.append(float(elt.split("/")[2]))
+            df["cdate_test_month"] = month
+            df["cdate_test_day"] = day
+            df["cdate_test_year"] = year
+
+        df[column + "_month"] = month
+        df[column + "_day"] = day
+        df[column + "_year"] = year
 
     return df
 
@@ -111,32 +182,31 @@ def birthdate_clean(df):
     return df
 
 
-def translate(column):
+def categorial_to_ordinal(df):
     """
-        Traduit le texte dans la colonne spécifiée
+        Transforme les données catégorielles (purpose, gender_x, status, gender_y, country, currency, city_restaurant) en données ordinales
     """
-    translator = Translator(to_lang="French")
 
-    i = 0
-    for text in df[column]:
-        text = translator.translate(str(text))
-        i += 1
-        if text.split(" ")[0] == "MYMEMORY" :
-            print("Limite atteinte %s")
-            break
+    categorial_list = ['purpose', 'gender_x', 'status', 'gender_y', 'country', 'currency', 'city_restaurant']
 
-    return df[column]
+    for column in categorial_list:
+        ce_ord = ce.OrdinalEncoder(cols = [column])
+        df["ord_" + column] = ce_ord.fit_transform(df[column])
+
+    return df
 
 
-def translate_chinese(df):
+def city_member_to_nan(df):
     """
-        Traduit les colonnes city_member, city_restaurant et opening_hours
+        Transforme correctement les données en NaN
     """
-    # ne traduire qu'une seule fois chaque récurrence
-    df["city_member"] = translate("city_member")
+    cities = []
+    for city in df["city_member"]:
+        if city == "0":
+            city = np.nan
+        cities.append(city)
 
-    # df["city_restaurant"] = translate("city_restaurant")
-    # df["opening_hours"] = translate("opening_hours")
+    df["city_member"] = cities
 
     return df
 
@@ -164,16 +234,69 @@ def number_of_nan(df):
     return dict
 
 
-if __name__ == '__main__':
-    df = merge_dataframe()
-    df = date_format(df)
-    df = birthdate_clean(df)
-    df = df.dropna()
-    # df = translate_chinese(df)
+def int_to_float(df):
 
+    people = []
+    prepay = []
+    return90 = []
+
+    for elt in df["people"]:
+        people.append(float(elt))
+    for elt in df["is_required_prepay_satisfied"]:
+        prepay.append(float(elt))
+    try:
+        for elt in df["return90"]:
+            return90.append(float(elt))
+    except:
+        pass
+
+    df["people"] = people
+    df["is_required_prepay_satisfied"] = prepay
+    try:
+        df["return90"] = return90
+    except:
+        pass
+
+    return df
+
+
+if __name__ == '__main__':
+    df_train = merge_dataframe("Train.csv")
+    df_test = merge_dataframe("Test.csv")
+
+####################### TRAIN ###########################
+
+    df_train = date_format(df_train)
+    df_train = birthdate_clean(df_train)
+    # df_train = city_member_to_nan(df_train)
+    df_train = df_train.dropna()
+    df_train = int_to_float(df_train)
+    df_train = drop_hours(df_train)
+    df_train = split_date(df_train)
+    df_train = categorial_to_ordinal(df_train)
+    # print(Index(df_train["city_member"].value_counts()).get_loc("9407"))
+
+    # print(type(df_train["ord_purpose"][0]))
+
+
+####################### TEST ###########################
+
+    df_test = date_format(df_test)
+    df_test = birthdate_clean(df_test)
+    # df_test = city_member_to_nan(df_test)
+    df_test = df_test.dropna()
+    df_test = int_to_float(df_test)
+    df_test = drop_hours(df_test)
+    df_test = split_date(df_test)
+    df_test = categorial_to_ordinal(df_test)
+
+########################################################
 
     # for k,v in number_of_nan(df).items():
     #     print(k, v)
 
-    print(df)
-    df.to_csv("./df.csv", index=False)
+    # print(df_train)
+    # print("--------------------------")
+    # print(df_test)
+    df_train.to_csv("./df_train.csv", index=False)
+    df_test.to_csv("./df_test.csv", index=False)
